602970c339038071bc8573028731415063ec03fa
FranciscoCharles <charlesdcf123@gmail.com>
2021-11-03 09:56:23 -0300

    melhorando a responsividade

--- script/footer.js
+++ script/footer.js
@@ -3,6 +3,6 @@
 	const year = today.getFullYear();
 	const footerTextElement = document.querySelector('.footer-container h1');
 	if(year > 2021){
-		footerTextElement.textContent.replace('2021', ` 2021 - ${year}`)
+		footerTextElement.textContent += ` - ${year}`
 	}
 })();

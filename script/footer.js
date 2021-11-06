3f35b53c19b9904736cd25c8ec75ecc8fcf19ab7
FranciscoCharles <charlesdcf123@gmail.com>
2021-11-06 18:08:27 -0300

    update codes

--- script/footer.js
+++ script/footer.js
@@ -1,7 +1,7 @@
 (function createFooterText(){
 	const today = new Date();
 	const year = today.getFullYear();
-	const footerTextElement = document.querySelector('.footer-container h1');
+	const footerTextElement = document.querySelector('.footer-container > .text-container > h1');
 	if(year > 2021){
 		footerTextElement.textContent += ` - ${year}`
 	}
